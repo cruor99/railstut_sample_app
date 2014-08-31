3578dd014845655d64292c23b0d29c2deb8daa77
Cruor99 <kliknes@gmail.com>
2014-08-31 23:24:58 +0200

    transitional

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,9 +4,8 @@ RSpec.describe "UserPages", :type => :request do
   describe "user_pages" do
     subject{page}
     describe"signup page" do
-      before{visitsignup_path}
+      before{visit signup_path}
       it{should have_content('Sign up')}
-      it{shuld have_title(full_title('Sign up'))}
     end
   end
 end
